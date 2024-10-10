5c0328d97c49220a33b505410d4f5d929e22026a
RobyLedda94 <robyledda01@yahoo.it>
2024-10-10 17:43:23 +0200

    [UPD] definito il technology seeder popolata la tabella nel db

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Technology extends Model
 {
     use HasFactory;
+
+    public static function generateSlug($title){
+        return Str::slug($title, '-');
+    }
 }
--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+// utilizzo il model technology
+use App\Models\Technology;
 
 class TechnologySeeder extends Seeder
 {
@@ -14,6 +16,13 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $technologies = ['Laravel', 'HTML', 'CSS', 'Javascript', 'PHP', 'Vue.js'];
+
+        foreach($technologies as $technology){
+            $new_technology = new Technology();
+            $new_technology->name = $technology;
+            $new_technology->slug = Technology::generateSlug($technology);
+            $new_technology->save();
+        }
     }
 }
