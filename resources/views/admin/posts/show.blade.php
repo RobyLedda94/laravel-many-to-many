abf45b5ddb7a529aebebe84e4e0a7715ad5d7566
RobyLedda94 <robyledda01@yahoo.it>
2024-10-13 14:15:07 +0200

    [UPD] mostro nella show le tecnologie assogiate al post

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -3,32 +3,33 @@
 @section('content')
     <div class="container-fluid p-4">
         <div class="row">
-            <!-- <div class="col-12">
-                <h2>{{$post->title}}</h2>
-                <img src="{{ asset('./storage/'. $post->cover_image) }}" alt="{{ $post->title }}">
-                <p>{{$post->slug}}</p>
-                <p>{{$post->content}}</p>
-            </div> -->
             <div class="card p-2" style="width: 18rem;">
-            @if (Str::startsWith($post->cover_image, 'https'))
-            <img class="img-fluid" src="{{ $post->cover_image }}" alt="{{ $post->title }}">
-            @else
-            <img class="img-fluid" src="{{ asset('./storage/'. $post->cover_image) }}" alt="{{ $post->title }}">
-            @endif
+                @if (Str::startsWith($post->cover_image, 'https'))
+                    <img class="img-fluid" src="{{ $post->cover_image }}" alt="{{ $post->title }}">
+                @else
+                    <img class="img-fluid" src="{{ asset('storage/' . $post->cover_image) }}" alt="{{ $post->title }}">
+                @endif
                 <div class="card-body">
-                    <h5 class="card-title">{{$post->title}}</h5>
-                    <p class="card-text">{{$post->slug}}</p>
-                    <p class="card-text">    
-                        @if($post->type)
+                    <h5 class="card-title">{{ $post->title }}</h5>
+                    <p class="card-text">{{ $post->slug }}</p>
+                    <p class="card-text">
+                        @if ($post->type)
                             {{ $post->type->name }}
                         @else
-                        <span class="text-danger">Tipo non disponibile</span>
+                            <span class="text-danger">Tipo non disponibile</span>
                         @endif
                     </p>
-                    <p class="card-text">{{$post->content}}</p>
+                    <p>
+                        @forelse ($post->technologies as $technology)
+                            {{ $technology->name }}
+                        @empty
+                            Il post non ha tag assegnati
+                        @endforelse
+                    </p>
+                    <p class="card-text">{{ $post->content }}</p>
                 </div>
                 <div class="card-footer text-muted">
-                        <small>Pubblicato il {{ $post->created_at->format('d/m/Y') }}</small>
+                    <small>Pubblicato il {{ $post->created_at->format('d/m/Y') }}</small>
                 </div>
             </div>
         </div>
